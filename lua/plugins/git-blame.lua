1346f0f1d10fe7eef9a7432349edb4f0e909f0ae
Yurii Lunov <lunyov.yu@gmail.com>
2025-02-06 22:58:57 +0200

    added format on save to lua

--- lua/plugins/git-blame.lua
+++ lua/plugins/git-blame.lua
@@ -1,11 +1,11 @@
 return {
-	"f-person/git-blame.nvim",
-	event = "VeryLazy",
-	opts = {
-		enabled = true,
-		message_template = " <author>, <date> • <summary>",
-		date_format = "%r",
-		virtual_text_column = 1,
-		max_commit_summary_length = 50,
-	},
+  "f-person/git-blame.nvim",
+  event = "VeryLazy",
+  opts = {
+    enabled = true,
+    message_template = " <author>, <date> • <summary>",
+    date_format = "%r",
+    virtual_text_column = 1,
+    max_commit_summary_length = 50,
+  },
 }
--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,11 +1,11 @@
 return {
-	"lewis6991/gitsigns.nvim",
-	config = function()
-		local gitsigns = require("gitsigns")
-		gitsigns.setup()
+  "lewis6991/gitsigns.nvim",
+  config = function()
+    local gitsigns = require("gitsigns")
+    gitsigns.setup()
 
-		vim.keymap.set("n", "<leader>hp", gitsigns.preview_hunk, { desc = "Hunk Preview" })
-		vim.keymap.set("n", "<leader>hs", gitsigns.stage_hunk, { desc = "Hunk Stage" })
-		vim.keymap.set("n", "<leader>hr", gitsigns.reset_hunk, { desc = "Hunk Reset" })
-	end,
+    vim.keymap.set("n", "<leader>hp", gitsigns.preview_hunk, { desc = "Hunk Preview" })
+    vim.keymap.set("n", "<leader>hs", gitsigns.stage_hunk, { desc = "Hunk Stage" })
+    vim.keymap.set("n", "<leader>hr", gitsigns.reset_hunk, { desc = "Hunk Reset" })
+  end,
 }
