89b6d109db0cb0e5bf0989e3d57cf20f2df20534
Yurii Lunov <lunyov.yu@gmail.com>
2025-02-12 19:32:19 +0200

    replaced linting and formatting with nvim-lint and confrom
    replaced linting with nvim-lint
    replaced formatting with confrom
    cleaned-up null_ls to provide only code actions

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -8,22 +8,8 @@ return {
 
 		null_ls.setup({
 			sources = {
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.prettierd,
-				require("none-ls.diagnostics.eslint_d"),
-				-- require("none-ls.formatting.eslint_d"),
 				require("none-ls.code_actions.eslint_d"),
 			},
-		})
-
-		vim.keymap.set("n", "<leader>fm", vim.lsp.buf.format, { desc = "Format" })
-
-		-- Run format on save
-		vim.api.nvim_create_autocmd("BufWritePre", {
-			pattern = { "*.js", "*.jsx", "*.ts", "*.tsx", "*.lua" },
-			callback = function()
-				vim.lsp.buf.format({ async = false })
-			end,
 		})
 	end,
 }
