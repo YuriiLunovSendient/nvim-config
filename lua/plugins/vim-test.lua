e53a20504295c2389e1a802dd5e38f0762f8693a
Yurii Lunov <lunyov.yu@gmail.com>
2025-08-26 20:17:26 +0300

    updated vim-tests config

--- lua/plugins/vim-test.lua
+++ lua/plugins/vim-test.lua
@@ -1,8 +1,17 @@
 return {
 	"vim-test/vim-test",
-
+	dependencies = { "preservim/vimux" },
 	config = function()
-		vim.g["test#strategy"] = "neovim"
+		vim.g["test#strategy"] = "vimux"
+
+		-- Vitest: match only *.unit.test.ts(x) or *.integration.test.ts(x)
+		vim.g["test#typescript#vitest#file_pattern"] = [[\v\.(unit|integration)\.test\.(ts|tsx)$]]
+		vim.g["test#javascript#vitest#file_pattern"] = [[\v\.(unit|integration)\.test\.(ts|tsx)$]]
+
+		-- Cypress: match only *.cy.ts(x)
+		vim.g["test#javascript#cypress#file_pattern"] = [[\v\.cy\.(ts|tsx|js|jsx)$]]
+		vim.g["test#typescript#cypress#file_pattern"] = [[\v\.cy\.(ts|tsx)$]]
+
 		vim.keymap.set("n", "<leader>tn", ":TestNearest<CR>")
 		vim.keymap.set("n", "<leader>tf", ":TestFile<CR>")
 		vim.keymap.set("n", "<leader>ts", ":TestSuite<CR>")
