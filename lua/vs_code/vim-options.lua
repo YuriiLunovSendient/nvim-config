7ec93794099206b57442f123c209d32d2fad2ea0
Yurii Lunov <lunyov.yu@gmail.com>
2025-12-07 19:22:17 +0200

    minor config update

--- lua/vs_code/vim-options.lua
+++ lua/vs_code/vim-options.lua
@@ -3,6 +3,20 @@ local vscode = require("vscode")
 vim.g.mapleader = " "
 vim.opt.clipboard = "unnamedplus"
 
+-- Commands
+-- Disable Cursor Tab in normal mode
+vim.api.nvim_create_autocmd("InsertLeave", {
+	callback = function()
+		require("vscode").action("editor.cpp.disableenabled")
+	end,
+})
+vim.api.nvim_create_autocmd("InsertEnter", {
+	callback = function()
+		require("vscode").action("editor.action.enableCppGlobally")
+	end,
+})
+
+-- Keybindings
 vim.keymap.set({ "n", "v" }, "\\", '"_', { noremap = true, silent = true, desc = "Black hole register" })
 
 vim.keymap.set({ "n", "v" }, "<leader>w", function()
@@ -52,3 +66,28 @@ end, { noremap = true, silent = true, desc = "Show Hover" })
 vim.keymap.set("n", "za", function()
 	vscode.action("editor.toggleFold")
 end, { noremap = true, silent = true, desc = "Toggle Fold" })
+
+-- Git Keybindings
+vim.keymap.set("n", "<leader>gb", function()
+	vscode.action("git.checkout")
+end, { noremap = true, silent = true, desc = "[G]it [B]ranch" })
+
+vim.keymap.set("n", "<leader>gnb", function()
+	vscode.action("git.branch")
+end, { noremap = true, silent = true, desc = "[G]it [N]ew [B]ranch" })
+
+vim.keymap.set("n", "<leader>gM", function()
+	vscode.action("git.merge")
+end, { noremap = true, silent = true, desc = "[G]it [M]erge" })
+
+vim.keymap.set("n", "<leader>gR", function()
+	vscode.action("git.rebase")
+end, { noremap = true, silent = true, desc = "[G]it [R]ebase" })
+
+vim.keymap.set("n", "<leader>gp", function()
+	vscode.action("git.pull")
+end, { noremap = true, silent = true, desc = "[G]it [P]ull" })
+
+vim.keymap.set("n", "<leader>gP", function()
+	vscode.action("git.push")
+end, { noremap = true, silent = true, desc = "[G]it [P]ush" })
