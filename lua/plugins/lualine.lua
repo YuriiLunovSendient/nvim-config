2594fe5e9252b871c3c2da229f013aa9ca513cb2
Yurii Lunov <lunyov.yu@gmail.com>
2026-01-26 22:55:15 +0200

    switch theme to auto, updated separators

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,7 +3,9 @@ return {
 	config = function()
 		require("lualine").setup({
 			options = {
-				theme = "catppuccin",
+				theme = "auto",
+				component_separators = "",
+				section_separators = { left = " ", right = " " },
 			},
 		})
 	end,
