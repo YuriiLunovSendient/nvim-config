2f875621aae7f030d50e5ca06ddc303db9537516
Yurii Lunov <lunyov.yu@gmail.com>
2026-04-10 17:13:04 +0300

    replaced nvim-web-devicons with mini.icons

--- plugin/render-markdown.lua
+++ plugin/render-markdown.lua
@@ -1,6 +1,5 @@
 vim.pack.add({
 	"https://github.com/nvim-treesitter/nvim-treesitter",
-	"https://github.com/nvim-tree/nvim-web-devicons",
 	"https://github.com/MeanderingProgrammer/render-markdown.nvim",
 })
 
